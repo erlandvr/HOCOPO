7c4569b3f06eb66e6898d109a13d14c4317e3731
Erlandvr <erlandvr@gmail.com>
2018-10-02 17:15:28 +0200

    enabled MVC, created routes, added about- and contactpage

--- Hocopo/Hocopo/Startup.cs
+++ Hocopo/Hocopo/Startup.cs
@@ -10,14 +10,27 @@ namespace Hocopo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseStaticFiles();
+
             app.UseNodeModules(env);
+
+            //UseMvc: listens to request and maps them to a Controller
+            app.UseMvc(cfg =>
+            //routes setup
+            {
+                cfg.MapRoute("Default", "/{controller}/{action}/{id?}",/*gives us a default route to chose when controller and action are missing in the url ->*/ new { controller = "App", Action = "Index" });
+            });
         }
     }
 }
